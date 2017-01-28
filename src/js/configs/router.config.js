0994554ada3ee629e88556acf006345c6040ec1f
sarahemilym <sarahemendham@gmail.com>
2017-01-28 14:41:10 +0000

    set up config folder

--- src/js/configs/router.config.js
+++ src/js/configs/router.config.js
@@ -6,7 +6,11 @@ Router.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
 function Router($stateProvider, $locationProvider, $urlRouterProvider) {
   $locationProvider.html5Mode(true);
 
-
+  $stateProvider
+    .state('home', {
+      url: '/',
+      template: <h1>Home</h1>;
+    })
 
 
 }
