8fb5a7fcb30851fa491dddb5041d261e31784729
remyce <remycewilsonhughes@gmail.com>
2017-01-28 15:08:40 +0000

    added routes and mongoose to index.js

--- api/config/routes.js
+++ api/config/routes.js
@@ -3,3 +3,14 @@ const router  = express.Router();
 const users   = require('../controllers/users');
 
 router.route('/users').get(users.index);
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
--- api/db/seeds.js
+++ api/db/seeds.js
@@ -7,10 +7,13 @@ const User = require('../models/user');
 User.collection.drop();
 
 const user = new User({
-  name: 'Remyce'
+  first_name: 'Tom',
+  last_name: 'Duckworth',
+  email: 'tom@tom.com',
+  age: '99'
 });
 
 user.save(function (err, user){
   if (err) return console.log(err);
-  console.log('user saved', user); 
+  console.log('user saved', user);
 });
