5e5f67a7923f8c38704233ec86ac9d462cb29e8a
remyce <remycewilsonhughes@gmail.com>
2017-01-29 20:27:41 +0000

    controllers events have been written

--- api/controllers/events.js
+++ api/controllers/events.js
@@ -1,32 +1,4 @@
-// router.route('/events')
-//   .get(events.index)
-//   .post(events.create);
-// router.route('events/:id')
-//   .get(events.show)
-//   .get(events.index)
-//   .put(events.update)
-//   .patch(events.update)
-//   .delete(events.delete);
-
-function eventsIndex(){
-
-}
-
-function eventsCreate(){
-
-}
-
-function eventsShow(){
-
-}
-
-function eventsUpdate(){
-
-}
-
-function eventsDelete(){
-
-}
+const Event = require('../models/event');
 
 module.exports = {
   index: eventsIndex,
@@ -35,3 +7,55 @@ module.exports = {
   update: eventsUpdate,
   delete: eventsDelete
 };
+
+function eventsIndex(req, res){
+  Event
+  .find({}, (err, event) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(event);
+  });
+}
+
+function eventsCreate(req, res){
+
+  // const event = new Event(req.body.event);
+
+  Event
+  .save((err, event) => {
+    if (err) return res.status(500).json(err);
+    return res.status(201).json(event);
+  });
+}
+
+function eventsShow(req, res){
+
+  const id = req.params.id;
+
+  Event
+  .findById({ _id: id })
+  .populate('events')
+  .exec((err, event) => {
+    if (err) return res.status(500).json(err);
+    if (!event) return res.status(404).json({ error: 'No event was found.' });
+    return res.status(200).json(event);
+  });
+}
+
+function eventsUpdate(req, res){
+  const id = req.params.id;
+
+  Event.findByIdAndUpdate({ _id: id }, req.body.event, (err, event) => {
+    if (err) return res.status(500).json(err);
+    if (!event) return res.status(404).json({ error: 'No event was found.' });
+    return res.status(200).json(event);
+  });
+}
+
+function eventsDelete(req, res){
+  const id = req.params.id;
+
+  Event.findByIdAndRemove({ _id: id }, err => {
+    if (err) return res.status(500).json(err);
+    return res.sendStatus(200);
+  });
+}
