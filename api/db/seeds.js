55bae6e4dc3de832f3f37c5ea9126f6054570155
gabrielghost <charlie.gavins@neonseed.com>
2017-01-29 21:36:49 +0000

    seed file tweaks

--- api/db/seeds.js
+++ api/db/seeds.js
@@ -11,42 +11,46 @@ const users = [
     first_name: 'Thom',
     last_name: 'Yorke',
     email: 'thom@yorke.com',
-    passwordHash: '',
     profile_picture: 'http://static.stereogum.com/uploads/2015/03/Thom-Yorke.jpg',
     DoB: '08/08/1968',
-    latlng: '',
+    latlng: 'filler',
     bio: 'Radiohead man',
-    events_attended: '',
-    events_requested: '',
-    events_flaked: '',
-    events_hosted: ''
+    events_attended: 'filler',
+    events_requested: 'filler',
+    events_flaked: 'filler',
+    events_hosted: 'filler',
+    password: 'password',
+    passwordConfirmation: 'password'
   },{
     first_name: 'Nils',
     last_name: 'Frahm',
     email: 'nils@frahm.com',
-    passwordHash: '',
     profile_picture: 'http://media.briterevolution.com/heroes/artist/thumb/2014/01/29/nilsfrahm.jpg',
     DoB: '20/09/1982',
-    latlng: '',
+    latlng: 'filler',
     bio: 'Piano man',
-    events_attended: '',
-    events_requested: '',
-    events_flaked: '',
-    events_hosted: ''
+    events_attended: 'filler',
+    events_requested: 'filler',
+    events_flaked: 'fillerfiller',
+    events_hosted: 'filler',
+    password: 'password',
+    passwordConfirmation: 'password'
   },{
     first_name: 'Brian',
     last_name: 'Eno',
     email: 'brian@eno.com',
-    passwordHash: '',
     profile_picture: 'http://www.electronicbeats.net/app/uploads/2015/03/BrianEno_EB.jpg',
     DoB: '15/05/1948',
-    latlng: '',
+    latlng: 'filler',
     bio: 'Ambient man',
-    events_attended: '',
-    events_requested: '',
-    events_flaked: '',
-    events_hosted: ''}
+    events_attended: 'filler',
+    events_requested: 'filler',
+    events_flaked: 'filler',
+    events_hosted: 'filler',
+    password: 'password',
+    passwordConfirmation: 'password'
+  }
 ];
 
-users.forEach(user => User.create(user, (err, user) => console.log(`${user.first_name}`)
+users.forEach(user => User.create(user, (err, user) => console.log(user)
 ));
