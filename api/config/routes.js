f2b5b7b30e0e37b5b86bd72e5ab81e91929aa94c
remyce <remycewilsonhughes@gmail.com>
2017-01-29 18:28:39 +0000

    added routes for events

--- api/config/routes.js
+++ api/config/routes.js
@@ -1,19 +1,28 @@
-const express = require('express');
-const router  = express.Router();
+const express         = require('express');
+const router          = express.Router();
 
 const authentications = require('../controllers/authentications');
-const users    = require('../controllers/users');
+const events          = require('../controllers/events');
+const users           = require('../controllers/users');
 
-router.route('/users')
-  .get(users.index);
+
+router.route('/events')
+  .get(events.index)
+  .post(events.create);
+router.route('events/:id')
+  .get(events.show)
+  .get(users.index)
+  .put(events.update)
+  .patch(events.update)
+  .delete(events.delete);
+
+router.route('users/:id')
+  .get(users.show);
+
 router.route('/register')
-  .post(authentications.register);
+.post(authentications.register);
 router.route('/login')
-  .post(authentications.login);
-
-
-
-
+.post(authentications.login);
 
 
 
