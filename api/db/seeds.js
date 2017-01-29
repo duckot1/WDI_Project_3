fc4ee4df5b677f5b76a32725c2727c5f16422bbe
gabrielghost <charlie.gavins@neonseed.com>
2017-01-29 11:22:42 +0000

    working on seed data and accompanying function

--- api/db/seeds.js
+++ api/db/seeds.js
@@ -6,14 +6,49 @@ const User = require('../models/user');
 
 User.collection.drop();
 
-const user = new User({
-  first_name: 'Tom',
-  last_name: 'Duckworth',
-  email: 'tom@tom.com',
-  age: '99'
-});
+const users = [
+  {first_name: 'Tom',
+    last_name: 'Duckworth',
+    email: 'tom@tom.com',
+    password: '',
+    profile_picture: '',
+    DoB: '',
+    latlng: '',
+    bio: '',
+    events_attended: '',
+    events_requested: '',
+    events_flaked: '',
+    events_hosted: ''},
+  {first_name: 'Charlie',
+    last_name: 'Gavins',
+    email: 'charlie@gavins.com',
+    password: 'password',
+    profile_picture: '',
+    DoB: '',
+    latlng: '',
+    bio: '',
+    events_attended: '',
+    events_requested: '',
+    events_flaked: '',
+    events_hosted: ''},
+  {first_name: 'Thom',
+    last_name: 'Yorke',
+    email: 'thom@yorke.com',
+    password: '',
+    profile_picture: '',
+    DoB: '',
+    latlng: '',
+    bio: '',
+    events_attended: '',
+    events_requested: '',
+    events_flaked: '',
+    events_hosted: ''}];
 
-user.save(function (err, user){
-  if (err) return console.log(err);
-  console.log('user saved', user);
-});
+
+
+for (let i=0;i<users.length; i++){
+  users[i].create(function(err, user) {
+    if (err) return console.log(err);
+    console.log('user saved', user);
+  });
+}
