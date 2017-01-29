ae30042cdc8089937a540c7482c51dca0a4dcffb
remyce <remycewilsonhughes@gmail.com>
2017-01-29 13:21:37 +0000

    merged changes

--- api/config/routes.js
+++ api/config/routes.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const router  = express.Router();
-const users   = require('../controllers/users');
+const express         = require('express');
+const router          = express.Router();
+const authentications = require('../controllers/authentications');
+const users           = require('../controllers/users');
 
 router.route('/users')
 .get(users.index);
+
+router.route('/register')
+.post(authentications.register);
+
+router.route('/login')
+.post(authentications.login);
+
 
 
 
