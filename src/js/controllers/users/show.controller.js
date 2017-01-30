100e54afba54e29f85eb0ea11a305c095077f02e
sarahemilym <sarahemendham@gmail.com>
2017-01-30 16:42:19 +0000

    added usersDelete function

--- src/js/controllers/users/show.controller.js
+++ src/js/controllers/users/show.controller.js
@@ -6,6 +6,17 @@ UsersShowCtrl.$inject = ['API', '$stateParams', 'User'];
 function UsersShowCtrl(API, $stateParams, User){
   const vm = this;
   vm.user = User.get($stateParams);
+  vm.delete = usersDelete;
 
   console.log(vm.user, 'show vm.user');
+
+  function usersDelete(user){
+    console.log(user, 'userDelete')
+    User
+      .delete({ id: user._id })
+      .$promise
+      .then(() => {
+        console.log('deleted');
+      });
+  }
 }
