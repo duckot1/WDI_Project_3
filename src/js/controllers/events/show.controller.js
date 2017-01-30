117232852ecf3c73980c28742f8726656064ff40
LucaScanu <lucagscanu@gmail.com>
2017-01-30 17:26:24 +0000

    removed public folder

--- src/js/controllers/events/show.controller.js
+++ src/js/controllers/events/show.controller.js
@@ -5,7 +5,7 @@ angular
 EventsShowCtrl.$inject = ['API','$http', '$stateParams', 'User', 'CurrentUserService'];
 function EventsShowCtrl(API, $http, $stateParams, User, CurrentUserService) {
   const vm = this;
-  vm.request = {};
+  vm.request;
 
   EventsShow();
 
@@ -21,7 +21,6 @@ function EventsShowCtrl(API, $http, $stateParams, User, CurrentUserService) {
       .delete(`${API}/events/${$stateParams.id}`);
   };
   vm.interest = function(host) {
-    console.log(host.event_host);
     vm.request.receiver_id = host.event_host;
     vm.request.sender_id = CurrentUserService.getUser()._id;
     vm.request.event_id = host._id;
