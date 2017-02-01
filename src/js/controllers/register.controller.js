9b478d74fce7276909f68ef804144607798d0cb9
gabrielghost <charlie.gavins@neonseed.com>
2017-02-01 10:54:16 +0000

    registerworking but no redirect

--- src/js/controllers/register.controller.js
+++ src/js/controllers/register.controller.js
@@ -2,17 +2,17 @@ angular
   .module('clubMate')
   .controller('UserRegisterCtrl', UserRegisterCtrl);
 
-UserRegisterCtrl.$inject = ['User', 'CurrentUserService'];
-function UserRegisterCtrl(User, CurrentUserService) {
+UserRegisterCtrl.$inject = ['User', 'CurrentUserService', '$state'];
+function UserRegisterCtrl(User, CurrentUserService, $state) {
   const vm = this;
-  vm.hello = 'hello';
   vm.register = () => {
     console.log(vm.user);
     User
     .register(vm.user)
     .$promise
-    .then(() => {
+    .then((response) => {
       CurrentUserService.getUser();
+      // $state.go('userShow', response.user._id);
     }, err => {
       console.log(err);
     });
