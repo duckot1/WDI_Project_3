42f1d6559128163f6ddd105611f63c0d1f313fbc
LucaScanu <lucagscanu@gmail.com>
2017-02-02 16:00:14 +0000

    about to add angular-slick

--- src/js/controllers/events/index.controller.js
+++ src/js/controllers/events/index.controller.js
@@ -5,14 +5,5 @@ angular
 EventsIndexCtrl.$inject = ['Event'];
 function EventsIndexCtrl(Event) {
   const vm  = this;
-  vm.slides = vm.events = Event.query();
-  vm.giveClass = giveClass;
-
-  function giveClass(index) {
-    if (index === 0) return 'active';
-  }
-
-  $('a.carousel-control').click((e) => {
-    e.preventDefault();
-  });
+  vm.events = Event.query();
 }
