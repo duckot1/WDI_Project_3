16ad891cf1ca4a0adcd7874ede9c5e609a88fc87
duckot1 <tom.duckworth91@gmail.com>
2017-02-02 19:44:40 +0000

    added hide and show to my events page

--- src/js/controllers/events/users-events.controller.js
+++ src/js/controllers/events/users-events.controller.js
@@ -6,10 +6,41 @@ UsersEventsCtrl.$inject = ['User', '$stateParams'];
 function UsersEventsCtrl(User, $stateParams) {
   const vm  = this;
   vm.title = 'My Events';
+  vm.eventsAttending = [];
+  vm.events;
+  vm.noEvents = true;
+  vm.checkAttending = false;
+  vm.checkHosting = false;
   User
-    .get($stateParams).$promise
-    .then(response => {
-      vm.events = response.events;
-      console.log(vm.events);
-    });
+  .get($stateParams).$promise
+  .then(response => {
+    vm.events = response.events;
+    if (vm.events.length === 0) {
+      vm.checkHosting = true;
+    } else {
+      vm.noEvents = false;
+    }
+  });
+
+  User.inbox($stateParams).$promise
+  .then(data => {
+    vm.messages = data;
+    if (vm.eventsAttending.length === 0) {
+      vm.checkAttending = true;
+    } else {
+      vm.noEvents = false;
+    }
+    for (var i = 0; i < vm.messages.length; i++) {
+      if (vm.messages[i].status === 'accepted') {
+        console.log(vm.eventsAttending, vm.messages[i]);
+        vm.eventsAttending.push(vm.messages[i]);
+      }
+    }
+  });
+
+
+
+
+
+
 }
